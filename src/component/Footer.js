001991d71fb7de97cd19b06e1560f6ccbee5346b
arvin karimi <hayitsarvink@gmail.com>
2025-02-17 06:40:21 -0500

    new map added 3

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -26,7 +26,8 @@ const Footer = () => {
                     </div>
             </div>
             <div className='google-map-div'>
-            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3237.10610505738!2d51.414026312009!3d35.772769872443476!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e0689f7d18999%3A0x21ed013cd321e531!2sTehran%20Province%2C%20Tehran%2C%20District%203%2C%20W%20Atefi%20St%2C%20Iran!5e0!3m2!1sen!2sit!4v1739791341467!5m2!1sen!2sit" width="600" height="450" style={{border: "0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            {/* <iframe title='googleMap' className='google-map' src="https://www.google.com/maps/embed?pb=!1m19!1m8!1m3!1d1619.344915115713!2d51.41539!3d35.733846!3m2!1i1024!2i768!4f13.1!4m8!3e0!4m0!4m5!1s0x3f8e0131020a7e99%3A0x985dd8f8628335ca!2sPCM8%2BJ9V%20District%206%2C%20Tehran%2C%20Tehran%20Province%2C%20Iran!3m2!1d35.734112499999995!2d51.4159219!5e0!3m2!1sen!2sus!4v1699784972105!5m2!1sen!2sus" allowFullScreen="" loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe> */}
+            <iframe  className='google-map' src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3237.10610505738!2d51.414026312009!3d35.772769872443476!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3f8e0689f7d18999%3A0x21ed013cd321e531!2sTehran%20Province%2C%20Tehran%2C%20District%203%2C%20W%20Atefi%20St%2C%20Iran!5e0!3m2!1sen!2sit!4v1739791341467!5m2!1sen!2sit" width="600" height="450" style={{border: "0"}} allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
             </div>
             <div className='footer-line-div'>
                     <div className='footer-white-line'></div>
